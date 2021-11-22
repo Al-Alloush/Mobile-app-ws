4e01b510cdf689e21b5accd80b1c927af4588449
Al-Allouhs <ahmad.alloush@gmail.com>
2021-11-22 14:57:38 +0100

    post user details to userController

--- src/main/java/com/dachser/app/ws/ui/controller/UserController.java
+++ src/main/java/com/dachser/app/ws/ui/controller/UserController.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dachser.app.ws.ui.model.request.UserDetailsRequestModel;
 
 @RestController // to receive HTTP request
 @RequestMapping("users") // http://localholst:8080/users
@@ -26,9 +29,9 @@ public class UserController {
 	}
 	
 	@PostMapping()
-	public String createUser() {
+	public String createUser(@RequestBody UserDetailsRequestModel userDetails ) {
 		
-		return "Create user methode";
+		return "Create user methode for user: " + userDetails.getFirstName();
 	}
 	
 	
